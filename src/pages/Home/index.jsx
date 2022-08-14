412260f0631a95afc9c909db4778e0c0a0a462d9
Kum Somi <kumsomi.5@gmail.com>
2022-08-14 14:27:01 +0530

    styling change

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,5 @@
-// import { useToast } from "react-toastify";
-// import { toast, ToastContainer} from "react-toastify";
 import { CategoryList, Header} from "../../Components";
-import { usePageTitle, useToast} from "../../custom-hooks";
-// 
-// import 'react-toastify/dist/ReactToastify.minimal.css';
+import { usePageTitle} from "../../custom-hooks";
 
 const Home=()=>{
     usePageTitle('Travel Tape | Home');
