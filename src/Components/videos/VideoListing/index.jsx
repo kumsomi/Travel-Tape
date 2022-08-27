a950f680329554b6dcde382d4f16082129ed0d1b
Kum Somi <kumsomi.5@gmail.com>
2022-08-27 19:16:44 +0530

    fixed single video

--- src/Components/videos/VideoListing/index.jsx
+++ src/Components/videos/VideoListing/index.jsx
@@ -2,9 +2,11 @@ import { VideoCard } from "../VideoCard";
 import "./style.css";
 
 const VideoListing=({videos, page})=>{
-    console.log(videos);
+    // console.log(videos);
     return(
-        <div className="video-container">
+        <div 
+        className="video-container"
+        >
             {videos.map((video)=>(
                 // <div className="video-listing">
                 <VideoCard key={video._id} video={video} page={page}/>
