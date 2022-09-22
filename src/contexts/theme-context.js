93fd46462ea6176aa80eef2343036d17cdfa01b0
Kum Somi <kumsomi.5@gmail.com>
2022-09-22 21:06:01 +0530

    updated default to light theme

--- src/contexts/theme-context.js
+++ src/contexts/theme-context.js
@@ -2,11 +2,11 @@ import { useContext } from "react";
 import { useState } from "react";
 import { createContext } from "react";
 
-const initialTheme={theme:'dark'};
+const initialTheme={theme:'light'};
 const ThemeContext=createContext(initialTheme);
 const {Provider}=ThemeContext;
 const ThemeProvider=({children})=>{
-    const [theme, setTheme]=useState(localStorage.getItem('travel-tape-theme')|| 'dark');
+    const [theme, setTheme]=useState(localStorage.getItem('travel-tape-theme')|| 'light');
     return <Provider value={{theme, setTheme}}>
         {children}
     </Provider>
